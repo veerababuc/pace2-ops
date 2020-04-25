55f1ae9bf7fc7eeee6fe146be3644db1a191c325
veerababuc <46822099+veerababuc@users.noreply.github.com>
2020-04-25 16:59:45 +0530

    commit

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -3,14 +3,15 @@ import { WorkordereditComponent } from './workorderedit/workorderedit';
 import { Pace2createnewserviceComponent } from './pace2createnewservice/pace2createnewservice';
 import { Pace2selectdepartmentComponent } from './pace2selectdepartment/pace2selectdepartment';
 import { FeadBackModule } from './feed-back/feed-bacm.module';
+import { workordereditModule } from './workorderedit/workordermodule';
 
 @NgModule({
-	declarations: [WorkordereditComponent,
+	declarations: [
     Pace2createnewserviceComponent,
     Pace2selectdepartmentComponent,
     ],
-	imports: [FeadBackModule,],
-	exports: [WorkordereditComponent,
+	imports: [FeadBackModule,workordereditModule],
+	exports: [
     Pace2createnewserviceComponent,
     Pace2selectdepartmentComponent,
     ],
