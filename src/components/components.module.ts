4a560c1bcfc1bfe8d498390e22c0a59aa22f4617
veerababuc <46822099+veerababuc@users.noreply.github.com>
2020-04-25 17:08:10 +0530

    commit

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -1,18 +1,16 @@
 import { NgModule } from '@angular/core';
-import { WorkordereditComponent } from './workorderedit/workorderedit';
-import { Pace2createnewserviceComponent } from './pace2createnewservice/pace2createnewservice';
 import { Pace2selectdepartmentComponent } from './pace2selectdepartment/pace2selectdepartment';
 import { FeadBackModule } from './feed-back/feed-bacm.module';
 import { workordereditModule } from './workorderedit/workordermodule';
+import { pace2createnewserviceModule } from './pace2createnewservice/pace2createnewservice.module';
 
 @NgModule({
 	declarations: [
-    Pace2createnewserviceComponent,
     Pace2selectdepartmentComponent,
     ],
-	imports: [FeadBackModule,workordereditModule],
+    imports: [FeadBackModule,workordereditModule,
+        pace2createnewserviceModule],
 	exports: [
-    Pace2createnewserviceComponent,
     Pace2selectdepartmentComponent,
     ],
     
---
+++ src/components/pace2createnewservice/pace2createnewservice.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { Pace2createnewserviceComponent } from './pace2createnewservice';
+
+@NgModule({
+  declarations: [
+    Pace2createnewserviceComponent,
+  ],
+  imports: [
+    IonicPageModule.forChild(Pace2createnewserviceComponent),
+  ],
+})
+export class pace2createnewserviceModule {}
