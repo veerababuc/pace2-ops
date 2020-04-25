cd231796e82473f824581c56aeb17f461337c1b6
veerababuc <46822099+veerababuc@users.noreply.github.com>
2020-04-25 17:25:08 +0530

    commit

--- src/pages/test/test.module.ts
+++ src/pages/test/test.module.ts
@@ -5,9 +5,10 @@ import {Pace2exceptionComponent} from '../../components/pace2exception/pace2exce
 import {Pace2addserviceComponent} from '../../components/pace2addservice/pace2addservice';
 import {Pace2notesComponent} from '../../components/pace2notes/pace2notes';
 import {Pace2deleteComponent} from '../../components/pace2delete/pace2delete';
-import {Pace2createnewserviceComponent} from '../../components/pace2createnewservice/pace2createnewservice';
+//import {Pace2createnewserviceComponent} from '../../components/pace2createnewservice/pace2createnewservice';
 import {Pace2selectdepartmentComponent} from '../../components/pace2selectdepartment/pace2selectdepartment';
 import { customselectModule } from '../../components/custom-select/custom-selectmodule';
+import { pace2createnewserviceModule } from '../../components/pace2createnewservice/pace2createnewservice.module';
 
 @NgModule({
   declarations: [
@@ -16,13 +17,14 @@ import { customselectModule } from '../../components/custom-select/custom-select
     Pace2addserviceComponent,
     Pace2notesComponent,
     Pace2deleteComponent,
-    Pace2createnewserviceComponent,
+    
     Pace2selectdepartmentComponent,
     
   ],
   imports: [
     IonicPageModule.forChild(TestPage),
-    customselectModule
+    customselectModule,
+    pace2createnewserviceModule
   ],
 })
 export class TestPageModule {}
