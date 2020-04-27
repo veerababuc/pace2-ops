4f7421d4e8955af9750063806f3fd80490b0bc83
veerababuc <46822099+veerababuc@users.noreply.github.com>
2020-04-27 22:03:48 +0530

    comit

--- src/components/pace2createnewservice/pace2createnewservice.module.ts
+++ src/components/pace2createnewservice/pace2createnewservice.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
-import { IonicPageModule } from 'ionic-angular';
+import { IonicModule } from 'ionic-angular';
 import { Pace2createnewserviceComponent } from './pace2createnewservice';
 
 @NgModule({
   declarations: [
     Pace2createnewserviceComponent,
   ],
-  imports: [],
+  imports: [IonicModule],
   exports:[Pace2createnewserviceComponent],
 })
 export class pace2createnewserviceModule {}
