d0f22dcf5c9d50b4fa331c906d145bce5b3e0b7e
veerababuc <46822099+veerababuc@users.noreply.github.com>
2020-04-27 13:13:39 +0530

    commit

--- src/pages/test/test.module.ts
+++ src/pages/test/test.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { TestPage } from './test';
-import {Pace2exceptionComponent} from '../../components/pace2exception/pace2exception';
-import {Pace2addserviceComponent} from '../../components/pace2addservice/pace2addservice';
+//import {Pace2exceptionComponent} from '../../components/pace2exception/pace2exception';
+//import {Pace2addserviceComponent} from '../../components/pace2addservice/pace2addservice';
 //import {Pace2notesComponent} from '../../components/pace2notes/pace2notes';
 //import {Pace2deleteComponent} from '../../components/pace2delete/pace2delete';
 //import {Pace2createnewserviceComponent} from '../../components/pace2createnewservice/pace2createnewservice';
-import {Pace2selectdepartmentComponent} from '../../components/pace2selectdepartment/pace2selectdepartment';
+//import {Pace2selectdepartmentComponent} from '../../components/pace2selectdepartment/pace2selectdepartment';
 import { customselectModule } from '../../components/custom-select/custom-selectmodule';
 import { pace2createnewserviceModule } from '../../components/pace2createnewservice/pace2createnewservice.module';
 import { Pace2deleteModule } from '../../components/pace2delete/pace2delete.module';
@@ -15,10 +15,10 @@ import { Pace2NotesModule } from '../../components/pace2notes/pace2notes.module'
 @NgModule({
   declarations: [
     TestPage,
-    Pace2exceptionComponent,
-    Pace2addserviceComponent,
+    //Pace2exceptionComponent,
+    //Pace2addserviceComponent,
    // Pace2deleteComponent,
-    Pace2selectdepartmentComponent,
+    //Pace2selectdepartmentComponent,
    // Pace2notesComponent
   ],
   imports: [
