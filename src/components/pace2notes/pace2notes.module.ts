7ad5835464bfdff94148ebe16e4dee9daa874f19
veerababuc <46822099+veerababuc@users.noreply.github.com>
2020-04-27 13:24:14 +0530

    commit

--- src/components/pace2notes/pace2notes.module.ts
+++ src/components/pace2notes/pace2notes.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
-import { IonicPageModule } from 'ionic-angular';
+import {  IonicModule } from 'ionic-angular';
 import { Pace2notesComponent } from './pace2notes';
 
 @NgModule({
     declarations: [Pace2notesComponent],
-    imports: [IonicPageModule.forChild(Pace2notesComponent)],
+    imports: [IonicModule],
     exports: [Pace2notesComponent],
     providers: []
 })
