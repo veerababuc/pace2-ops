16f11d055d8849b876652f3687ccb1db8ce8271b
veerababuc <46822099+veerababuc@users.noreply.github.com>
2020-04-28 00:27:27 +0530

    commit

--- src/components/pace2exception/pace2exception.module.ts
+++ src/components/pace2exception/pace2exception.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import {  IonicModule } from 'ionic-angular';
 import { Pace2exceptionComponent } from './pace2exception';
+import { Pace2addserviceModule } from '../pace2addservice/pace2addservice.module';
 
 @NgModule({
     declarations: [Pace2exceptionComponent],
-    imports: [IonicModule],
+    imports: [IonicModule,Pace2addserviceModule],
     exports: [Pace2exceptionComponent],
     providers: []
 })
