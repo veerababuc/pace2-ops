061e202878fb6c534e2d496a2662a62cfb699f13
veerababuc <46822099+veerababuc@users.noreply.github.com>
2020-04-27 19:43:29 +0530

    commit

---
+++ src/components/pace2addservice/pace2addservice.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import {  IonicModule } from 'ionic-angular';
+import { Pace2addserviceComponent } from './pace2addservice';
+
+@NgModule({
+    declarations: [Pace2addserviceComponent],
+    imports: [IonicModule],
+    exports: [Pace2addserviceComponent],
+    providers: []
+})
+export class Pace2addserviceModule { }
