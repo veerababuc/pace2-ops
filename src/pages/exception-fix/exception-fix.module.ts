1c9c612e107726db14b14d48af7a36532105ea67
veerababuc <46822099+veerababuc@users.noreply.github.com>
2020-04-28 10:03:19 +0530

    commit

--- src/pages/exception-fix/exception-fix.module.ts
+++ src/pages/exception-fix/exception-fix.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { ExceptionFixPage } from './exception-fix';
-import { Pace2addserviceModule } from '../../components/pace2addservice/pace2addservice.module';
+//import { Pace2addserviceModule } from '../../components/pace2addservice/pace2addservice.module';
 
 @NgModule({
   declarations: [
@@ -9,7 +9,7 @@ import { Pace2addserviceModule } from '../../components/pace2addservice/pace2add
   ],
   imports: [
     IonicPageModule.forChild(ExceptionFixPage),
-    Pace2addserviceModule
+   // Pace2addserviceModule
   ],
 })
 export class ExceptionFixPageModule {}
